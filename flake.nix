b516c94a6d50c000245c94d7fcfd82b8610afe1d
StepBroBD <Hi@StepBroBD.com>
2023-09-15 23:35:02 +0200

    project init

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  inputs = {
+    nixpkgs.url = "flake:nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "flake:flake-utils";
+    gomod2nix = {
+      url = "github:nix-community/gomod2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    , gomod2nix
+    , ...
+    }: flake-utils.lib.eachDefaultSystem
+      (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            gomod2nix.overlays.default
+          ];
+        };
+
+        lib = pkgs.lib;
+
+        finch = pkgs.buildGoApplication rec {
+          name = "finch";
+          src = lib.cleanSource ./.;
+          modules = ./gomod2nix.toml;
+        };
+      in
+      {
+        packages.default = finch;
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            go_1_21
+            gopls
+            gomod2nix.packages.${system}.default
+          ];
+        };
+
+        formatter = pkgs.nixpkgs-fmt;
+      }
+      );
+}
