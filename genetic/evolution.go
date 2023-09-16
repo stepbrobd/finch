7262fae913491c9c05a84fd6d6053c370bb4ddb7
pehringer <pehringer@outlook.com>
2023-09-16 10:51:08 +0200

    Split up monolithic file.

---
+++ genetic/evolution.go
@@ -0,0 +1,22 @@
+package genetic
+
+
+func Evolve(gens, num int, sizes []int, examInputs, expecOutputs [][]float32, rate float32, err float64) {
+	gen := 0
+	term := false
+	curErr := 0.0
+	p := Init(num, sizes)
+	for gen < gens && term == false {
+		p.SelectCrossReplace()
+		p.Mutate(rate)
+		p.FitEval(examInputs, expecOutputs)
+		p.Terminate(err, &curErr, &term)
+		fmt.Printf("Gen: %d, Err: %f\n", gen, curErr)
+		gen++
+	}
+	fmt.Println("Ins:  EXP: ACT:")
+	for i := 0; i < len(examInputs); i++ {
+		act := p.Nets[0].ForProp(examInputs[i])
+		fmt.Println(examInputs[i], expecOutputs[i], "", act[:1])
+	}
+}
