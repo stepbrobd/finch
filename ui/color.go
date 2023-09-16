f153b27aa9fa2205727baae9882f86c1bdea01c8
StepBroBD <Hi@StepBroBD.com>
2023-09-16 09:10:24 +0200

    add lipgloss

---
+++ ui/color.go
@@ -0,0 +1,13 @@
+package ui
+
+import (
+	lipgloss "github.com/charmbracelet/lipgloss"
+)
+
+const (
+	Red    = lipgloss.Color("#ff0000")
+	Blue   = lipgloss.Color("#0070f3")
+	Pink   = lipgloss.Color("#f81ce5")
+	Green  = lipgloss.Color("#50e3c2")
+	Yellow = lipgloss.Color("#f5a623")
+)
