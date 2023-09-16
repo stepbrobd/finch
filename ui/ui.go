54346c9830555e0d05c60f585c20d65aed2d8323
Yifei Sun <ysun@hey.com>
2023-09-16 18:58:27 +0200

    full screen support and responsive

--- ui/ui.go
+++ ui/ui.go
@@ -1,41 +1,64 @@
 package ui
 
 import (
+	"fmt"
+	"log"
+	"time"
+
 	"github.com/stepbrobd/finch/genetic"
 
+	stopwatch "github.com/charmbracelet/bubbles/stopwatch"
 	tea "github.com/charmbracelet/bubbletea"
+	lipgloss "github.com/charmbracelet/lipgloss"
+	term "golang.org/x/term"
 )
 
 type Model struct {
 	Model genetic.Algo
+	Watch stopwatch.Model
 }
 
 func InitialModel(algo *genetic.Algo) Model {
 	return Model{
 		Model: *algo,
+		Watch: stopwatch.NewWithInterval(time.Second),
 	}
 }
 
 func (m Model) Init() tea.Cmd {
-	return nil
+	return tea.Batch(
+		m.Watch.Init(),
+		tea.EnterAltScreen,
+	)
 }
 
 func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
-		// q, ctrl+c
-		case "q", "ctrl+c":
+		// esc, ctrl+c
+		case "esc", "ctrl+c":
 			return m, tea.Quit
-
 		}
 	}
+	var cmd tea.Cmd
+	m.Watch, cmd = m.Watch.Update(msg)
 
-	return m, nil
+	return m, cmd
 }
 
 func (m Model) View() string {
-	view := StyleBlue500.Render("Finch\n")
+	w, _, err := term.GetSize(0)
+	if err != nil {
+		log.Fatalf("failed to get terminal size: %v", err)
+	}
 
-	return view
+	view := lipgloss.NewStyle().
+		Bold(true).
+		Underline(true).
+		Align(lipgloss.Center).
+		Render("Finch") + "\n\n"
+	view += "Elapsed: " + m.Watch.View() + fmt.Sprintf(" Generation: %d", (m.Model.GetNumGens())) + "\n\n"
+
+	return lipgloss.PlaceHorizontal(w, lipgloss.Center, view)
 }
